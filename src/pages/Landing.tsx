64c7d9ae9ebc1e4e03a173abfb33974535a592f5
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-09-21 21:16:31 -0400

    Add rough Features component

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,13 @@
-const Landing = () => {
-    return (
-        <>
-            <h1>Landing</h1>
-        </>
-    );
+import React from 'react';
+import { Features } from '@components';
+
+const Landing: React.FC = () => {
+  return (
+    <>
+      <h1>Landing</h1>
+      <Features />
+    </>
+  );
 };
 
 export { Landing };
