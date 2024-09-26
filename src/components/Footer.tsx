ded696b149355fd5d0bbdb0ed611707802e10f42
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-09-25 21:24:04 -0400

    Create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p className="footer-copyright">Maven Sourcing &copy; {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
