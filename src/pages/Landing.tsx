ff6a7955a020a1b0c864ca7bab333c6fa5e1c710
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-09-25 22:42:51 -0400

    Remove h1 tag

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { Navbar, Features, Footer } from '@components';
 
 const Landing: React.FC = () => {
-  return (
-    <>
-      <Navbar />
-      <h1>Landing</h1>
-      <Features />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Features />
+            <Footer />
+        </>
+    );
 };
 
-export { Landing }
+export { Landing };
