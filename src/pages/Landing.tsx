039b488fb356c124b90e7dc691222e8e37526ad0
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-09-25 21:24:14 -0400

    Add footer to landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Features } from '@components';
+import { Features, Footer } from '@components';
 
 const Landing: React.FC = () => {
   return (
     <>
       <h1>Landing</h1>
       <Features />
+      <Footer />
     </>
   );
 };
